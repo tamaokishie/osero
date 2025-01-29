6b8c9b4dcada7d7027a071acb572e38f6b926f7f
tamaokishie <shie0041@gmail.com>
2025-01-30 02:06:37 +0900

    add css

--- src/Board.tsx
+++ src/Board.tsx
@@ -1,21 +1,25 @@
-import React, { useState } from "react"
+import React, { useState } from "react";
+import "./Board.css";
 
 export const Board = () => {
 
-    let number = [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ];
+    let count = [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ];
 
     const [clickedNum, setClickedNum] = useState<number>(0);
 
-    const handleClick = (num: number) => {
-        setClickedNum(num);
+    const handleClick = (cnt: number) => {
+        setClickedNum(cnt);
     };
 
     return(
         <div>
-            {number.map((num, index) => (
+            {count.map((cnt, index) => (
                 <React.Fragment key={index}>
-                    <button onClick={() => handleClick(num)}>
-                        {clickedNum === num ? clickedNum :''}
+                    <button
+                        className = "button"
+                        onClick={() => handleClick(cnt)}
+                    >
+                        {clickedNum === cnt ? clickedNum : null}
                     </button>
                     {(index + 1) % 3 === 0 && <br />}
                 </React.Fragment>
