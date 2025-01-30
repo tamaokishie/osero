efe0286e7fbd8fbd863d7fa6f2d91921e68ac6ed
tamaokishie <shie0041@gmail.com>
2025-01-31 00:02:34 +0900

    fix css

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { App } from "./App.tsx";
+import "./index.css";
 import { Board } from "./Board.tsx";
 
 const root = ReactDOM.createRoot(document.getElementById("root")!); // "root"はHTMLファイルのid属性
